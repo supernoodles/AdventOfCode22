55ce932e92f1e7273cb88885fd03f18895a1400d
Steve Cusack <sdcusack@googlemail.com>
2022-12-12 12:04:38 +0000

    Day1 part 1

---
+++ Day1/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var input = File.ReadAllText("input.txt");
+
+using var reader = new StringReader(input);
+
+var line = "";
+
+int maxHealth = 0;
+
+int currentHealth = 0;
+
+while (line != null)
+{
+    line = reader.ReadLine();
+
+    if(string.IsNullOrWhiteSpace(line))
+    {
+        if(currentHealth > maxHealth)
+        {
+            maxHealth = currentHealth;
+        }
+
+        currentHealth = 0;
+
+        continue;
+    }
+
+    currentHealth += int.Parse(line);
+}
+
+if(currentHealth > maxHealth)
+{
+    maxHealth = currentHealth;
+}
+
+Console.WriteLine(maxHealth);
