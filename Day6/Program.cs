f86fc9c64ee6bc4dc708b386c0112ccb7443c7f7
Steve Cusack <sdcusack@googlemail.com>
2023-01-02 18:50:22 +0000

    Day 6 - Part 1

---
+++ Day6/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Hello, AdventOfCode Day 6!");
+
+var input = File.ReadAllText("input.txt");
+
+var inputTest = """
+mjqjpqmgbljsphdztnvjfqwrcgsmlb
+""";
+
+Console.WriteLine($"Part1 = {Part1(input)}");
+
+Console.WriteLine($"Part2 = {Part2(input)}");
+
+int Part1(string input)
+{
+    for (var index = 0; index < input.Length; ++index)
+    {
+        var markerCandidate = input.Skip(index).Take(4);
+
+        if(!markerCandidate.GroupBy(_ => _).Any(_ => _.Count() > 1))
+        {
+            return index + 4;
+        }
+    }
+    
+    return 0;
+}
+
+int Part2(string input)
+{
+    return 0;
+}
