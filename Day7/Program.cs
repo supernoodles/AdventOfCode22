4771fd5b6d2fe5856f861180b0739d6044d7b0af
Steve Cusack <sdcusack@googlemail.com>
2023-01-07 11:03:10 +0000

    Day 7 - Part 1

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine("Hello, AdventOfCode Day 5!");
+﻿Console.WriteLine("Hello, AdventOfCode Day 7!");
 
-var input = File.ReadAllText("input.txt");
+var input = System.IO.File.ReadAllText("input.txt");
 
 var inputTest = """
 $ cd /
@@ -43,7 +43,44 @@ int Part1(string input)
 {
     var lines = InputToLineList(input);
 
-    return 0;
+    Folder root = new ("/", null);
+
+    var currentFolder = root;
+
+    foreach (var line in lines)
+    {
+        var lineParts = line.Split(" ");
+
+        if (line == "$ cd /" || line == "$ ls")
+        {
+            continue;
+        }
+
+        if (line.StartsWith("dir "))
+        {
+            var folder = lineParts[1];
+            currentFolder.SubFolders.Add(new Folder(folder, currentFolder));
+            continue;
+        }
+
+        if (line.StartsWith("$ cd "))
+        {
+            var folder = lineParts[2];
+
+            currentFolder = folder == ".."
+                ? currentFolder.Parent
+                : currentFolder = currentFolder.SubFolders.Where(f => f.Name == folder).First();
+            
+            continue;
+        }
+
+        var file = new File(lineParts[1], int.Parse(lineParts[0]));
+        currentFolder.Files.Add(file);
+    }
+
+    Console.WriteLine(root.CalcSize());
+
+    return FindTotalWhereFolderLt100000(root);
 }
 
 int Part2(string input)
@@ -51,21 +88,45 @@ int Part2(string input)
     return 0;
 }
 
+int FindTotalWhereFolderLt100000(Folder folder)
+{
+    var subs = folder.SubFolders.Sum(_ => FindTotalWhereFolderLt100000(_));
+
+    return folder.TotalSize < 100000
+        ? folder.TotalSize + subs
+        : subs;
+}
+
 class Folder
 {
     public string Name { get; set; }
 
-    public Folder Parent { get; set; }
+    public Folder? Parent { get; set; }
 
     public List<Folder> SubFolders { get; set; }
 
-    public List<int> FileSizes {get; set;}
+    public List<File> Files {get; set;}
 
-    public Folder(string name, Folder parent)
+    public int TotalSize { get; set; }
+
+    public Folder(string name, Folder? parent)
     {
         Name = name;
         Parent = parent;
         SubFolders = new ();
-        FileSizes = new ();
+        Files = new ();
+    }
+
+    public int CalcSize()
+    {
+        var fileTotal = Files.Sum(file => file.Size);
+
+        var dirTotal = SubFolders.Sum(_ => _.CalcSize());
+
+        TotalSize = fileTotal + dirTotal;
+
+        return TotalSize;
     }
 }
+
+record File (string Name, int Size);
